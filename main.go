85d55741c6d3eeb72a5e38f0fcccc46ce6571d59
likhithkp <likhithkpdev@gmail.com>
2025-02-16 11:00:25 +0530

    Practice4

--- main.go
+++ main.go
@@ -1,31 +1,5 @@
 package main
 
-import "fmt"
-
-type Function interface {
-	Drive()
-}
-
-type Car struct{}
-
-func (c Car) Drive() {
-	fmt.Println("Car is driving")
-}
-
-type Bike struct{}
-
-func (c Bike) Drive() {
-	fmt.Println("Bike is riding")
-}
-
-func doOperation(i Function) {
-	i.Drive()
-}
-
 func main() {
-	c := Car{}
-	b := Bike{}
 
-	doOperation(c)
-	doOperation(b)
 }
